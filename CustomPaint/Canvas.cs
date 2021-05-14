dbdc0ba931032803793aa930210aacaeee0ea67d
roma_ <roma_r9@mail.ru>
2021-05-14 14:16:31 +0500

    Custom Paint on Windows Forms

---
+++ CustomPaint/Canvas.cs
@@ -0,0 +1,211 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using System;
+
+
+//TODO Zoom
+
+namespace CustomPaint
+{
+    public partial class Canvas : Form
+    {
+        private bool drawing;
+        private int oldX, oldY;
+        private Bitmap bmp;
+        private int zoom = 1;
+
+        public int Zoom
+        {
+            get
+            {
+                return zoom;
+            }
+            set
+            {
+                if ((zoom < 6) && (zoom > 1))
+                {
+                    zoom = value;
+                }
+            }
+        }
+
+        public int CanvasWidth
+        {
+            get
+            {
+                return pictureBox1.Width;
+            }
+            /*set
+            {
+                pictureBox1.Width = value;
+                Bitmap tbmp = new Bitmap(value, pictureBox1.Height);
+                Graphics g = Graphics.FromImage(tbmp);
+                g.Clear(Color.White);
+                g.DrawImage(bmp, new Point(0, 0));
+                bmp = tbmp;
+                pictureBox1.Image = bmp;
+            }*/
+        }
+        public int CanvasHeight
+        {
+            get
+            {
+                return pictureBox1.Height;
+            }
+            /*set
+            {
+                pictureBox1.Height = value;
+                Bitmap tbmp = new Bitmap(pictureBox1.Width, value);
+                Graphics g = Graphics.FromImage(tbmp);
+                g.Clear(Color.White);
+                g.DrawImage(bmp, new Point(0, 0));
+                bmp = tbmp;
+                pictureBox1.Image = bmp;
+            }*/
+        }
+
+        public Canvas()
+        {
+            InitializeComponent();
+            bmp = new Bitmap(ClientSize.Width, ClientSize.Height);
+            pictureBox1.Image = bmp;
+        }
+
+        public Canvas(String FileName)
+        {
+            InitializeComponent();
+            bmp = new Bitmap(FileName);
+            Graphics g = Graphics.FromImage(bmp);
+            pictureBox1.Width = bmp.Width;
+            pictureBox1.Height = bmp.Height;
+            pictureBox1.Image = bmp;
+        }
+
+        public void ResizeCanvas(int width, int height)
+        {
+            pictureBox1.Width = width;
+            pictureBox1.Height = height;
+            Bitmap tbmp = new Bitmap(width, height);
+            Graphics g = Graphics.FromImage(tbmp);
+            g.Clear(Color.White);
+            g.DrawImage(bmp, new Point(0, 0));
+            bmp = tbmp;
+            pictureBox1.Image = bmp;
+        }
+
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            switch (MainForm.currentTool)
+            {
+                case MainForm.Tool.Кисточка:
+                    if (e.Button == MouseButtons.Left)
+                    {
+                        Graphics graphics = Graphics.FromImage(bmp);
+                        graphics.DrawLine(new Pen(MainForm.currentColor, MainForm.currentWidth), oldX, oldY, e.X, e.Y);
+                        oldX = e.X;
+                        oldY = e.Y;
+                        pictureBox1.Invalidate();
+                    }
+                    break;
+                case MainForm.Tool.Ластик:
+                    if (e.Button == MouseButtons.Left)
+                    {
+                        Graphics graphics = Graphics.FromImage(bmp);
+                        graphics.DrawLine(new Pen(pictureBox1.BackColor, MainForm.currentWidth), oldX, oldY, e.X, e.Y);
+                        oldX = e.X;
+                        oldY = e.Y;
+                        pictureBox1.Invalidate();
+                    }
+                    break;
+                case MainForm.Tool.Эллипс:
+                    if (drawing)
+                    {
+                        Graphics graphics = pictureBox1.CreateGraphics();
+                        graphics.DrawEllipse(new Pen(MainForm.currentColor, MainForm.currentWidth), oldX, oldY, e.X - oldX, e.Y - oldY);
+                        pictureBox1.Invalidate();
+                    }
+                    break;
+                case MainForm.Tool.Линия:
+                    if (drawing)
+                    {
+                        Graphics graphics = pictureBox1.CreateGraphics();
+                        graphics.DrawLine(new Pen(MainForm.currentColor, MainForm.currentWidth), oldX, oldY, e.X, e.Y);
+                        pictureBox1.Invalidate();
+                    }
+                    break;
+                case MainForm.Tool.Звезда:
+                    if (drawing)
+                    {
+                        Graphics graphics = pictureBox1.CreateGraphics();
+                        graphics.DrawPolygon(new Pen(MainForm.currentColor, MainForm.currentWidth), GetStarPoints(e));
+                        pictureBox1.Invalidate();
+                    }
+                    break;
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)   
+        {
+            if (MainForm.Tool.Эллипс == MainForm.currentTool)
+            {
+                Graphics graphics = Graphics.FromImage(bmp);
+                graphics.DrawEllipse(new Pen(MainForm.currentColor, MainForm.currentWidth), oldX, oldY, e.X - oldX, e.Y - oldY);
+                pictureBox1.Invalidate();
+            }
+            if (MainForm.Tool.Линия == MainForm.currentTool)
+            {
+                Graphics graphics = Graphics.FromImage(bmp);
+                graphics.DrawLine(new Pen(MainForm.currentColor, MainForm.currentWidth), oldX, oldY, e.X, e.Y);
+                pictureBox1.Invalidate();
+            }
+            if (MainForm.Tool.Звезда == MainForm.currentTool)
+            {
+                Graphics graphics = Graphics.FromImage(bmp);
+                graphics.DrawPolygon(new Pen(MainForm.currentColor, MainForm.currentWidth), GetStarPoints(e));
+                pictureBox1.Invalidate();
+            }
+            drawing = false;
+        }
+
+        private void pictureBox1_MouseDown_1(object sender, MouseEventArgs e)
+        {
+            oldX = e.X;
+            oldY = e.Y;
+            drawing = true;
+        }
+
+        private PointF[] GetStarPoints(MouseEventArgs e)
+        {
+
+            double BRadius, SRadius;
+            BRadius = Math.Sqrt((oldX - e.X) * (oldX - e.X) + (oldY - e.Y) * (oldY - e.Y));
+            SRadius = BRadius / 2.0;
+            double x0 = oldX, y0 = oldY;
+            int n = MainForm.NStar;
+            double alpha = 0;
+            double a = alpha, da = Math.PI / n, l;
+            PointF[] points = new PointF[2*n + 1];
+
+
+            for (int k = 0; k < 2 * n + 1; k++)
+            {
+                l = k % 2 == 0 ? BRadius : SRadius;
+                points[k] = new PointF((float)(x0 + l * Math.Cos(a)), (float)(y0 + l * Math.Sin(a)));
+                a += da;
+            }
+
+            return points;
+        }
+
+        public void ZoomPicture(Size size)
+        {
+            Bitmap bm = new Bitmap(bmp, Convert.ToInt32(bmp.Width * size.Width),
+                Convert.ToInt32(bmp.Height * size.Height));
+            Graphics gpu = Graphics.FromImage(bm);
+            
+            pictureBox1.Image = null;
+            pictureBox1.Image = bm;
+        }
+    }
+}
---
+++ CustomPaint/MainForm.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomPaint
+{
+    public partial class MainForm : Form
+    {
+        private string currentFileName;
+        public enum Tool { Кисточка, Ластик, Эллипс, Линия, Звезда};
+        public static Color currentColor = Color.Black;
+        public static int currentWidth = 3;
+        public static Tool currentTool = 0;
+        public static int NStar = 4;
+        private Canvas canvas;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            colorButton.BackColor = currentColor;
+        }
+
+        private void красныйToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            currentColor = Color.Red;
+            colorButton.BackColor = currentColor;
+        }
+
+        private void зеленыйToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            currentColor = Color.LightGreen;
+            colorButton.BackColor = currentColor;
+        }
+
+        private void синийToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            currentColor = Color.Blue;
+            colorButton.BackColor = currentColor;
+        }
+
+        private void другойToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            colorDialog.FullOpen = true;
+            if (colorDialog.ShowDialog() == DialogResult.Cancel)
+                return;
+            currentColor = colorDialog.Color;
+            colorButton.BackColor = currentColor;
+        }
+
+        private void новыйРисунокToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            canvas = new Canvas();
+            canvas.MdiParent = this;
+            canvas.Show();
+        }
+
+        private void brushButton_Click(object sender, EventArgs e)
+        {
+            currentTool = 0;
+            toolLabel.Text = currentTool.ToString();
+        }
+
+        private void eraserButton_Click(object sender, EventArgs e)
+        {
+            currentTool = (Tool) 1;
+            toolLabel.Text = currentTool.ToString();
+        }
+
+        private void circleButton_Click(object sender, EventArgs e)
+        {
+            currentTool = (Tool) 2;
+            toolLabel.Text = currentTool.ToString();
+        }
+
+        private void lineButton_Click(object sender, EventArgs e)
+        {
+            currentTool = (Tool) 3;
+            toolLabel.Text = currentTool.ToString();
+        }
+
+        private void starButton_Click(object sender, EventArgs e)
+        {
+            currentTool = (Tool) 4;
+            toolLabel.Text = currentTool.ToString();
+            starVertex star = new starVertex();
+            star.Show();    
+        }
+
+        private void справкаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AboutPaint aboutPaint = new AboutPaint();
+            aboutPaint.Show();
+        }
+
+        private void рисунокToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            размерХолстаToolStripMenuItem.Enabled = !(ActiveMdiChild == null);
+        }
+
+        private void размерХолстаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CanvasSize cs = new CanvasSize(canvas.CanvasWidth, canvas.CanvasHeight);
+            cs.ShowDialog();
+            /*((Canvas)ActiveMdiChild).CanvasHeight = cs.canvasHeight;
+            ((Canvas)ActiveMdiChild).CanvasWidth = cs.canvasWidth;*/
+            canvas.ResizeCanvas(cs.canvasWidth, cs.canvasHeight);
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            canvas.Zoom++;
+            canvas.ZoomPicture(new Size(canvas.Zoom, canvas.Zoom));
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            canvas.Zoom--;
+            canvas.ZoomPicture(new Size(canvas.Zoom, canvas.Zoom));
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Save()
+        {
+
+        }
+
+        private void SaveAs()
+        {
+            saveFileDialog1.Filter = "JPEG Image(*.jpeg)|*.jpeg|JPG image(*.jpg)|*.jpg|Windows Bitmap(*.bmp)|*.bmp";
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            string filename = saveFileDialog1.FileName;
+            MessageBox.Show(saveFileDialog1.FileName.ToString());
+            /*canvas.pictureBox1.Image.Save(filename);*/
+        }
+
+        private void OpenImage()
+        {
+            openFileDialog1.Filter = "JPEG Image(*.jpeg)|*.jpeg|JPG image(*.jpg)|*.jpg|Windows Bitmap(*.bmp)|*.bmp";
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            currentFileName = openFileDialog1.FileName;
+            canvas = new Canvas(currentFileName);
+            canvas.MdiParent = this;
+            canvas.Show();
+        }
+
+        private void открытьФайлToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenImage();
+        }
+
+        private void сохранитьКакToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveAs();
+        }
+
+        /*Image ZoomPicture(Image img, Size size)
+        {
+            Bitmap bm = new Bitmap(img, Convert.ToInt32(img.Width * size.Width),
+                Convert.ToInt32(img.Height * size.Height));
+            Graphics gpu = Graphics.FromImage(bm);
+            gpu.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+            return bm;
+        }*/
+    }
+}
